e5eba467ed06dbe15b07da427406990159154bce
Renan Costa <renandev.rj@gmail.com>
2026-01-09 10:56:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
-import model.entities.Departament;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     static void main(String[] args) {
 
-        Departament obj = new Departament(1, "Books");
+        Department obj = new Department(1, "Books");
         IO.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        IO.println(seller);
 
     }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,7 +3,7 @@ package model.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Departament implements Serializable {
+public class Department implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -13,9 +13,9 @@ public class Departament implements Serializable {
 
 
 
-    public Departament(){
+    public Department(){
     }
-    public Departament(Integer id, String name) {
+    public Department(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
@@ -40,7 +40,7 @@ public class Departament implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Departament that = (Departament) o;
+        Department that = (Department) o;
         return Objects.equals(id, that.id);
     }
     @Override
